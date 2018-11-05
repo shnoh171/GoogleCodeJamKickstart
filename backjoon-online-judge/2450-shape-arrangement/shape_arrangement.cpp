db8c493ede46be5a6d7ffcf76bc54efba27d7a72
shnoh171 <shnoh171@gmail.com>
2018-11-05 11:28:56 +0900

    finish 2450

--- backjoon-online-judge/2450-shape-arrangement/shape_arrangement.cpp
+++ backjoon-online-judge/2450-shape-arrangement/shape_arrangement.cpp
@@ -6,29 +6,53 @@
 using namespace std;
 
 // https://www.acmicpc.net/problem/2450
-// Incomplete
 
-void split_to_int(const string& s, const char* delim, vector<int>& v);
+int min_num_of_swap(const int& i, const int& j, const int& k, vector<int>& shapes, int shapes_info[3]);
 
 int main()
 {
-	string s;
 	int size;
 	cin >> size;
 
 	vector<int> shapes;
-	split_to_int
+	int shapes_info[4] = { 0, };
+
+	for (int i = 0; i != size; ++i) {
+		int shape;
+		cin >> shape;
+
+		shapes.push_back(shape);
+		++shapes_info[shape];
+	}
+
+	int res =  min_num_of_swap(1, 2, 3, shapes, shapes_info);
+	res = min(res, min_num_of_swap(1, 3, 2, shapes, shapes_info));
+	res = min(res, min_num_of_swap(2, 1, 3, shapes, shapes_info));
+	res = min(res, min_num_of_swap(2, 3, 1, shapes, shapes_info));
+	res = min(res, min_num_of_swap(3, 1, 2, shapes, shapes_info));
+	res = min(res, min_num_of_swap(3, 2, 1, shapes, shapes_info));
+
+	cout << res << endl;
+
 	return 0;
 }
 
-void split_to_int(const string& s, const char* delim, vector<int>& v)
-{
-	char *dup = strdup(s.c_str());
-	char *token = strtok(dup, delim);
+int min_num_of_swap(const int& i, const int& j, const int& k, vector<int>& shapes, int shapes_info[3]) {
+	int count_j_in_i = 0, count_k_in_i = 0;
+	int count_j_in_k = 0, count_k_in_j = 0;
 
-	while (token != NULL) {
-		v.push_back(atoi(token));
-		token = strtok(NULL, delim);
+	for (int l = 0; l < shapes_info[i]; ++l) {
+		if (shapes[l] == j) ++count_j_in_i;
+		if (shapes[l] == k) ++count_k_in_i;
 	}
-	delete(dup);
+
+	for (int l = shapes_info[i]; l < shapes_info[i] + shapes_info[j]; ++l) {
+		if (shapes[l] == k) ++count_k_in_j;
+	}
+
+	for (int l = shapes_info[i] + shapes_info[j]; l < shapes.size(); ++l) {
+		if (shapes[l] == j) ++count_j_in_k;
+	}
+	
+	return count_j_in_i + count_k_in_i + max(count_j_in_k, count_k_in_j);
 }
