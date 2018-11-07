fc7134f6365209efbb065d318a0ee6830f85ac1c
shnoh171 <shnoh171@gmail.com>
2018-11-07 14:23:40 +0900

    add atcoder

---
+++ atcoder/atcoder-beginner-contest-113/B-palace/palace.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int N, T, A;
+	cin >> N >> T >> A;
+
+	vector<int> H;
+	for (int i = 0; i < N; ++i) {
+		int tmp;
+		cin >> tmp;
+		H.push_back(tmp);
+	}
+
+	double avg_temp = T - H[0] * 0.006;
+	int index = 0;
+
+	for (int i = 1; i < H.size(); ++i) {
+		double curr_avg_temp = T - H[i] * 0.006;
+		if (fabs(avg_temp - A) > fabs(curr_avg_temp - A)) {
+			avg_temp = curr_avg_temp;
+			index = i;
+		}
+	}
+
+	cout << index + 1 << endl;
+
+	return 0;
+}
