6a0e1735a13aa565161d95b44894b70288c0ff3c
shnoh171 <shnoh171@gmail.com>
2018-11-13 19:23:29 +0900

    Sieve of Eratosthenes

---
+++ backjoon-online-judge/1929-find-prime-numbers-m-n/find_prime_numbers_m_n.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// https://www.acmicpc.net/problem/1929
+// Sieve of Eratosthenes
+// Why timeout? I don't understand...
+
+int main()
+{
+	int m, n;
+	cin >> m >> n;
+
+	bool primes[n+1];
+	fill_n(primes, n+1, true);
+
+	primes[0] = false; primes[1] = false;
+
+	for (int i = 2; i*i <= n; ++i) {
+		if (primes[i] == true) {
+			for (int j = 2*i; j <= n; j += i) {
+				primes[j] = false;
+			}
+		}
+	}
+
+	for (int i = m; i <= n; ++i)
+		if (primes[i] == true)
+			cout << i << endl;
+
+	return 0;
+}
