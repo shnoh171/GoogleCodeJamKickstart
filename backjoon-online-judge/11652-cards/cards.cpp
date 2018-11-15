23141d2bbe4256fec016dc969f81ad892ddc67a2
shnoh171 <shnoh171@gmail.com>
2018-11-15 21:50:57 +0900

    sort, but also can use hash

--- backjoon-online-judge/11652-cards/cards.cpp
+++ backjoon-online-judge/11652-cards/cards.cpp
@@ -1,17 +1,39 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 // https://www.acmicpc.net/problem/11652
 
 int main()
 {
-	long long a = 1;
+	ios_base::sync_with_stdio(false);
 
-	int array[100000000000];
+	int n;
+	cin >> n;
+	
+	long long nums[n];
+	for (int i = 0; i < n; ++i) {
+		cin >> nums[i];
+	}
 
-	for (int i = 0; i < 62; ++i) a *=2;
+	sort(nums, nums+n);
+	
+	int curr = 0;
+	while (curr < n && nums[0] == nums[curr]) ++curr;
+	long long most_freq_num = nums[0];
+	int most_freq_cnt = curr;
 
-	cout << a << endl;
+	for (int i = curr; i < n+1; ++i) {
+		if (i >= n || nums[curr] != nums[i]) {
+			if (most_freq_cnt < i - curr) {
+				most_freq_num = nums[curr];
+				most_freq_cnt = i - curr;
+			}
+			curr = i;
+		}
+	}	
+
+	cout << most_freq_num << "\n";
 
 	return 0;
 }
