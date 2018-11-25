0583d7b6c4d1e81c508f780e944609453e72eb65
shnoh171 <shnoh171@gmail.com>
2018-11-25 20:17:00 +0900

    incomplete

--- backjoon-online-judge/contest/2014-pacific-northwest-region-programming-contest-division-2/10563-number-game/number_game.cpp
+++ backjoon-online-judge/contest/2014-pacific-northwest-region-programming-contest-division-2/10563-number-game/number_game.cpp
@@ -52,6 +52,46 @@ int main()
 
 		cout << left_able << " " << left_unable << " " << right_unable << " " << right_able << endl;
 
+		int left_last = left_able + right_unable + right_able;
+		int right_last = left_able + left_unable + right_able;
+
+		cout << left_last << " " << right_last << endl;
+
+		if (left_last % 2 == 1 && right_last % 2 == 1) {
+			cout << "Bob\n";
+		} else if (left_last % 2 == 0 && right_last % 2 == 0) {
+			cout << "Alice\n";
+		} else {
+			int *left = one - 1;
+			int *right = one + 1;
+
+			if (right != numbers+n) {
+				while (right+1 != numbers+n && *right < *(right+1)) {
+					++right;
+				}
+			}
+
+			if (left != numbers-1) {
+				while (left-1 != numbers-1 && *left < *(left-1)) {
+					--left;
+				}
+			}
+
+			int left_size, right_size;
+
+			if (one == numbers) left_size = 0;
+			else left_size = one - left - 1;
+
+			if (one == numbers + n + 1) right_size = 0;
+			else right_size = right - one - 1;
+
+			if (left_last % 2 == 1 && left_size > right_size)
+				cout << "Bob\n";
+			else if (right_last % 2 == 1 && right_size > left_size)
+				cout << "Bob\n";
+			else
+				cout << "Alice\n";
+		}
 	}
 
 	return 0;
