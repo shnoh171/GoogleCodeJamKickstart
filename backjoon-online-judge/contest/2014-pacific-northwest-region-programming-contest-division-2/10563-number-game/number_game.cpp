80873be32202d2b5daee91e815ff0255b6982bd0
shnoh171 <shnoh171@gmail.com>
2018-11-26 10:38:18 +0900

    incomplete

--- backjoon-online-judge/contest/2014-pacific-northwest-region-programming-contest-division-2/10563-number-game/number_game.cpp
+++ backjoon-online-judge/contest/2014-pacific-northwest-region-programming-contest-division-2/10563-number-game/number_game.cpp
@@ -40,28 +40,40 @@ int main()
 			}
 		}
 
-		int left_able, left_unable, right_able, right_unable;
+
+		int num_when_left_first;
+		int num_when_right_first;
+
+		if (right - one == 2)
+			num_when_left_first = n - 1;
+		else
+			num_when_left_first = n - (right - one - 1);
+
+		if (one - left == 2)
+			num_when_right_first = n - 1;
+		else
+			num_when_right_first = n - (one - left - 1);
+
+		cout << "num_when_left_first = " << num_when_left_first << endl;
+		cout << "num_when_right_first = " << num_when_right_first << endl;
+
 		if (one == numbers) {
-			left_able = 0;
-			left_unable = 0;
-		} else {
-			left_able = left - numbers + 1;
-			left_unable = one - left - 1;
+			if (num_when_left_first % 2 == 0)
+				cout << "Alice\n";
+			else
+				cout << "Bob\n";
+			return 0;
+		} else if (one == numbers + n - 1) {
+			if (num_when_right_first % 2 == 0)
+				cout << "Alice\n";
+			else
+				cout << "Bob\n";
+			return 0;
 		}
-		if (one == numbers + n - 1) {
-			right_able = 0;
-			right_unable = 0;
-		} else {
-			right_able = (numbers + n) - right;
-			right_unable = right - one - 1;
-		}
 
-		cout << left_able << " " << left_unable << " " << right_unable << " " << right_able << endl;
 
-		int left_last = left_able + right_unable + right_able;
-		int right_last = left_able + left_unable + right_able;
+		/*
 
-		cout << left_last << " " << right_last << endl;
 
 		if (left_last % 2 == 1 && right_last % 2 == 1) {
 			cout << "Bob\n";
@@ -98,6 +110,7 @@ int main()
 			else
 				cout << "Alice\n";
 		}
+		*/
 	}
 
 	return 0;
