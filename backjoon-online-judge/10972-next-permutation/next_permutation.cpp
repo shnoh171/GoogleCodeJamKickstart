8a840059c3aeee7c28053e42271a2953c4a7ab2b
shnoh171 <shnoh171@gmail.com>
2018-12-01 00:15:45 +0900

    permutation

--- backjoon-online-judge/10972-next-permutation/next_permutation.cpp
+++ backjoon-online-judge/10972-next-permutation/next_permutation.cpp
@@ -15,18 +15,59 @@ int main()
 
 	int list[n];
 	for (int i = 0; i < n; ++i) cin >> list[i];
-
-	if (next_permutation(list, list+n))
+	
+	if (NextPermutation(list, n))
 		for (int i = 0; i < n; ++i) cout << list[i] << " ";
 	else
 		cout << "-1";
 
 	cout << "\n";
+	/*
+	int list[n];
+	for (int i = 0; i < n; ++i) list[i] = i+1;;
+
+	int list_cmp[n];
+	for (int i = 0; i < n; ++i) list_cmp[i] = i+1;;
+
+	do {
+		for (int i = 0; i < n; ++i)
+			if (list[i] != list_cmp[i]) {
+				cout << "list: ";
+				for (int j = 0; j < n; ++j) cout << list[j] << " ";
+				cout << "\n";
+				cout << "list_cmp: ";
+				for (int j = 0; j < n; ++j) cout << list_cmp[j] << " ";
+				cout << "\n";
+				cout << "previous: ";
+				prev_permutation(list_cmp, list_cmp+n);
+				for (int j = 0; j < n; ++j) cout << list_cmp[j] << " ";
+				cout << "\n";
+
+				return 1;
+			}
+		next_permutation(list_cmp, list_cmp+n);
+	} while(NextPermutation(list, n)); 
+	*/
 
 	return 0;
 }
 
 bool NextPermutation(int list[], int n)
 {
+	int pivot = n-1;
+	while (pivot > 0 && list[pivot] < list[pivot-1])
+		--pivot;
 
+	if (pivot == 0) return false;
+
+	int index = n-1;
+	while (list[pivot-1] > list[index]) --index;
+
+	swap(list[pivot-1],list[index]);
+
+	for (int i = 0; i < (n - pivot) / 2; ++i) {
+		swap(list[pivot+i], list[n-1-i]);
+	}
+
+	return true;
 }
