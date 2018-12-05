1bd4679f0905e3ef8810ccefbbae2a4238c9402f
shnoh171 <shnoh171@gmail.com>
2018-12-05 17:56:35 +0900

    incomplete

---
+++ backjoon-online-judge/3111-sensorship/sensorship.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string word, text;
+	cin >> word >> text;
+
+	return 0;
+}
