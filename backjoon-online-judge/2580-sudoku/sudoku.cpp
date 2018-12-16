1af25064d53e1dd438cd2f28b16a6405e145a5c3
shnoh171 <shnoh171@gmail.com>
2018-12-16 18:40:22 +0900

    backtracking incomplete

---
+++ backjoon-online-judge/2580-sudoku/sudoku.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+int board[9][9];
+
+bool Solve(int idx);
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	for (int i = 0; i < 9; ++i) {
+		for (int j = 0; j < 9; ++j) {
+			cin >> board[i][j];
+		}
+	}
+
+	Solve(0);
+
+	return 0;
+}
+
+bool Solve(int idx)
+{
+	int y = idx / 9;
+	int x = idx % 9;
+
+	if (idx == 81) {
+		for (int i = 0; i < 9; ++i) {
+			for (int j = 0; j < 9; ++j) {
+				cout << board[i][j];
+				if (j != 8) cout << " ";
+			}
+			cout << "\n";
+		}
+		return true;
+	}
+
+	if (board[y][x] != 0) {
+		bool ret = Solve(idx+1);
+		if (ret) return true;
+	}
+
+	set<int> row, col, squ;
+
+	for (int i = 0; i < 9; ++i) {
+		int num = board[y][i];
+		if (num != 0) {
+			row.insert(num);
+		}
+	}
+
+	for (int i = 0; i < 9; ++i) {
+		int num = board[i][x];
+		if (num != 0) {
+			col.insert(num);
+		}
+	}
+	
+	for (int i = 0; i < 3; ++i) {
+		for (int j = 0; j < 3; ++j) {
+			int num = board[y-y%3+i][x-x%3+j];
+			if (num != 0) {
+				squ.insert(num);
+			}
+		}
+	}
+
+	vector<int> cands;
+	for (int i = 1; i < 10; ++i)
+		if (row.find(i) == row.end() &&
+		    col.find(i) == col.end() &&
+		    squ.find(i) == squ.end())
+			cands.push_back(i);
+
+	for (int i = 0; i < cands.size(); ++i) {
+		board[y][x] = cands[i];
+		bool ret = Solve(idx+1);
+		if (ret) return true;
+		board[y][x] = 0;
+	}
+	return false;
+}
