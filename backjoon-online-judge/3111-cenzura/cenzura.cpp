b2f77656ccf43be987167093f3cb10fd023f5374
unknown <silksky7@seoul.ac.kr>
2018-12-20 21:19:50 +0900

    stack

--- backjoon-online-judge/3111-cenzura/cenzura.cpp
+++ backjoon-online-judge/3111-cenzura/cenzura.cpp
@@ -1,10 +1,12 @@
+//#define FILEIO
+//#define WAIT
 #include <iostream>
 #include <algorithm>
 #include <stack>
 #include <fstream>
 using namespace std;
 
-void PopIfExist(stack<char> &s, string word);
+bool PopIfExist(stack<char> &s, string word);
 
 int main()
 {
@@ -12,15 +14,19 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	
+#ifdef FILEIO
 	ifstream fin;
 	ofstream fout;
-	fin.open("test/cenzura.in.7");
+	fin.open("test/cenzura.in.8");
 	fout.open("test/res.out");
-	
+#endif	
 
 	string a, t;
+#ifdef FILEIO
 	fin >> a >> t;	
+#else
+	cin >> a >> t;
+#endif
 	string a_rev = a;
 	reverse(a_rev.begin(), a_rev.end());
 
@@ -33,14 +39,15 @@ int main()
 	while (l <= r) {
 		if (is_left) {
 			left.push(t[l]);
-			PopIfExist(left, a_rev);
+			if (PopIfExist(left, a_rev))
+				is_left = !is_left;
 			++l;
 		} else {
 			right.push(t[r]);
-			PopIfExist(right, a);
+			if (PopIfExist(right, a))
+				is_left = !is_left;
 			--r;
 		}
-		is_left = !is_left;
 	}
 
 	int count = 0;
@@ -49,28 +56,39 @@ int main()
 		left.pop();
 		right.push(c);
 		if (count++ < a.size()) {
-			PopIfExist(right, a);
-			count = 0;
+			if (PopIfExist(right, a))
+				count = 0;
 		}
 	}
 
 	if (right.empty()) return 0;
 
 	while (!right.empty()) {
+#ifdef FILEIO
 		fout << right.top();
+#else
+		cout << right.top();
+#endif
 		right.pop();
 	}
-	fout << "\n";
 
+#ifdef FILEIO
+	fout << "\n";
+#else
+	cout << "\n";
+#endif
+
+#ifdef WAIT
 	cout << flush;
 	system("pause");
+#endif
 
 	return 0;
 }
 
-void PopIfExist(stack<char> &s, string word)
+bool PopIfExist(stack<char> &s, string word)
 {
-	if (s.size() < word.size()) return;
+	if (s.size() < word.size()) return false;
 
 	string tmp;
 	for (int i = 0; i < word.size(); ++i) {
@@ -79,9 +97,13 @@ void PopIfExist(stack<char> &s, string word)
 		tmp += c;
 	}
 
-	if (tmp == word) return;
+	if (tmp == word) {
+		return true;	
+	} 
 
 	for (int i = 0; i < word.size(); ++i) {
 		s.push(tmp[word.size()-1-i]);
 	}
+
+	return false;
 }
