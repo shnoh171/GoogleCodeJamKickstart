11c9a9f4f0a7c1793b2a76df4fe4c45fd9de1025
shnoh171 <shnoh171@gmail.com>
2018-12-21 21:42:11 +0900

    backtracking

--- backjoon-online-judge/2580-sudoku/sudoku.cpp
+++ backjoon-online-judge/2580-sudoku/sudoku.cpp
@@ -41,45 +41,47 @@ bool Solve(int idx)
 	}
 
 	if (board[y][x] != 0) {
-		bool ret = Solve(idx+1);
+		bool ret = Solve(idx + 1);
 		if (ret) return true;
+		else return false;
 	}
 
-	set<int> row, col, squ;
+	//set<int> row, col, squ;
+	int bitset = 0;
 
 	for (int i = 0; i < 9; ++i) {
 		int num = board[y][i];
 		if (num != 0) {
-			row.insert(num);
+			//row.insert(num);
+			bitset |= 1 << num;
 		}
 	}
 
 	for (int i = 0; i < 9; ++i) {
 		int num = board[i][x];
 		if (num != 0) {
-			col.insert(num);
+			//col.insert(num);
+			bitset |= 1 << num;
 		}
 	}
-	
+
 	for (int i = 0; i < 3; ++i) {
 		for (int j = 0; j < 3; ++j) {
-			int num = board[y-y%3+i][x-x%3+j];
+			int num = board[y - y % 3 + i][x - x % 3 + j];
 			if (num != 0) {
-				squ.insert(num);
+				bitset |= 1 << num;
 			}
 		}
 	}
 
 	vector<int> cands;
 	for (int i = 1; i < 10; ++i)
-		if (row.find(i) == row.end() &&
-		    col.find(i) == col.end() &&
-		    squ.find(i) == squ.end())
+		if (!(bitset & 1 << i))
 			cands.push_back(i);
 
 	for (int i = 0; i < cands.size(); ++i) {
 		board[y][x] = cands[i];
-		bool ret = Solve(idx+1);
+		bool ret = Solve(idx + 1);
 		if (ret) return true;
 		board[y][x] = 0;
 	}
