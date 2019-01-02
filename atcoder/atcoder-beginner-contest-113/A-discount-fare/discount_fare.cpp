cb47ae3f5c83facfaa7a004354d253931502776a
shnoh171 <shnoh171@gmail.com>
2019-01-02 15:04:24 +0900

    too easy

---
+++ atcoder/atcoder-beginner-contest-113/A-discount-fare/discount_fare.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int x, y;
+	cin >> x >> y;
+
+	cout << x + y / 2 << "\n";
+	
+	return 0;
+}
