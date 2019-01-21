7653f982ffee9b291e3ebc43d5b35a8dcde0a022
shnoh171 <shnoh171@gmail.com>
2019-01-22 00:17:52 +0900

    bfs (incomplete)

--- codeforces/codeforces-round-533-div2/D-kilani-and-the-game/kilani_and_the_game.cpp
+++ codeforces/codeforces-round-533-div2/D-kilani-and-the-game/kilani_and_the_game.cpp
@@ -1,29 +1,18 @@
 #include <iostream>
 #include <string>
 #include <queue>
+#include <cstring>
 using namespace std;
 
-// Incomplete - Solve Later
-
-struct node {
-	int y;
-	int x;
-	int dist;
-	node(int a, int b, int c) { y = a; x = b; dist = c; }
-};
+// Incomplete
 
 const int dy[4] = { -1, 0, 0, 1 };
 const int dx[4] = { 0, -1, 1, 0 };
 
-int n, m, p;
-int s[10];
-char map[1000][1000];
-int dist[1000][1000];
-queue<node> q[10];
 int cnt[10];
 
 bool Round(int pid);
-bool IsRange(int y, int x) { return y >= 0 && x >= 0 && y < n && y < m; }
+bool IsRange(int y, int x, int n, int m) { return y >= 0 && x >= 0 && y < n && x < m; }
 
 int main()
 {
@@ -31,7 +20,14 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
+	int n, m, p;
 	cin >> n >> m >> p;
+	char map[n][m];
+	int dist[n][m];
+	int s[p+1], cnt[p+1];
+	memset(cnt, 0, (p+1)*sizeof(int));
+	queue<pair<int, int>> q1, q2;
+	vector<pair<int, int>> init[10];
 	for (int i = 1; i <= p; ++i) cin >> s[i];
 	for (int i = 0; i < n; ++i) {
 		string s;
@@ -40,20 +36,40 @@ int main()
 			map[i][j] = s[j];
 			if (s[j] != '.' && s[j] != '#') {
 				dist[i][j] = 0;
-				int num = s[j] - '0';
-				q[num].push(node(i, j, 0));
+				init[s[j]-'0'].push_back(make_pair(i, j));
 			}
 		}
 	}
 
-	bool finish = false;
-	while (!finish) {
-		bool progress = false;
-		for (int i = 1; i <= p; ++i) {
-			bool tmp = Round(i);
-			progress = tmp || progress;
+	for (int i = 1; i < 10; ++i) {
+		for (auto x : init[i]) {
+			q2.push(x);
 		}
-		finish = !progress;
+	}
+
+	while (!q2.empty() || !q1.empty()) {
+		queue<pair<int, int>> &q = q1.empty() ? q2 : q1;
+
+		pair<int, int> curr = q.front();
+		int y = curr.first;
+		int x = curr.second;
+		int id = map[y][x] - '0';
+		q.pop();
+
+		++cnt[id];
+
+		for (int i = 0; i < 4; ++i) {
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			if (IsRange(ny, nx, n, m) && map[ny][nx] == '.') {
+				map[ny][nx] = id + '0';
+				dist[ny][nx] = (dist[y][x] + 1) % s[id];
+				if (dist[ny][nx])
+					q1.push(make_pair(ny, nx));
+				else
+					q2.push(make_pair(ny, nx));
+			}
+		}
 	}
 
 	/*
@@ -65,15 +81,6 @@ int main()
 	}
 	cout << "\n";
 	*/
-
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < m; ++j) {
-			if (map[i][j] != '.' && map[i][j] != '#') {
-				++cnt[map[i][j]-'0'];
-			}
-		}
-	}
-
 	for (int i = 1; i <= p; ++i)
 		cout << cnt[i] << " ";
 	cout << "\n";
@@ -81,37 +88,3 @@ int main()
 	return 0;
 }
 
-bool Round(int pid)
-{
-	if (q[pid].empty()) return false;
-
-	int count = 0;
-	int dist_limit = q[pid].front().dist + s[pid];
-
-	while (!q[pid].empty() && q[pid].front().dist < dist_limit) {
-		node curr = q[pid].front();
-		int y = curr.y;
-		int x = curr.x;
-		q[pid].pop();
-
-		++count;
-		//++cnt[pid];
-		
-		for (int i = 0; i < 4; ++i) {
-			int next_y = y + dy[i];
-			int next_x = x + dx[i];
-			int next_dist = curr.dist + 1;
-
-			if (IsRange(next_y, next_x)) {
-				if (map[next_y][next_x] == '.') {
-					map[next_y][next_x] = pid + '0';
-					dist[next_y][next_x] = next_dist;
-					q[pid].push(node(next_y, next_x, next_dist));
-				}
-			}
-		}
-	}
-
-	if (count == 0) return false;
-	else return true;
-}
