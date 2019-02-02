c7f40982b4cac90a997cd3010d38d502906082f5
shnoh171 <shnoh171@gmail.com>
2019-02-03 00:49:23 +0900

    dp

--- backjoon-online-judge/2240-plum-tree/plum_tree.cpp
+++ backjoon-online-judge/2240-plum-tree/plum_tree.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 int d[1001][31][2];
-int ComputeMax(int time, int change, int tree, int seq[]);
 
 int main()
 {
@@ -13,36 +12,54 @@ int main()
 
 	int t, w;
 	cin >> t >> w;
-	int seq[t];
+	int seq[t+1];
 	for (int i = 1; i <= t; ++i) {
 		int id;
 		cin >> id;
 		seq[i] = --id; // {1, 2} -> {0, 1}
 	}
 
-	int res;
-	if (t - 1 <= w) {
-		res = t;
-	} else {
-		res = max(ComputeMax(t, w, 0, seq), ComputeMax(t, w, 1, seq));
+	d[0][0][0] = 0;
+	d[0][0][1] = -1;
+	for (int i = 1; i <= t; ++i) {
+		if (seq[i] == 0) d[i][0][0] = d[i-1][0][0] + 1;
+		else d[i][0][0] = d[i-1][0][0];
+		d[i][0][1] = -1;		
 	}
 
-	cout << res << "\n";
+	d[1][1][0] = -1;
+	d[1][1][1] = (seq[1] == 1);
+
+	for (int j = 2; j <= w; ++j) {
+		d[1][j][0] = d[1][j][1] = -1;
+	}
+
+	for (int i = 2; i <= t; ++i) {
+		for (int j = 1; j <= w; ++j) {
+			for (int k = 0; k <= 1; ++k) {
+				int cand1 = d[i-1][j-1][(k+1)%2];
+				int cand2 = d[i-1][j][k];
+
+				if (cand1 < 0 && cand2 < 0) {
+					d[i][j][k] = -1;
+				} else if (cand1 < 0) {
+					d[i][j][k] = cand2 + (k == seq[i]);
+				} else if (cand2 < 0) {
+					d[i][j][k] = cand1 + (k == seq[i]);
+				} else {
+					d[i][j][k] = max(cand1, cand2) + (k == seq[i]);
+				}
+			}
+		}
+	}
+
+	int res = 0;
+	for (int j = 0; j <= w; ++j)
+		for (int k = 0; k <= 1; ++k)
+			res = max(res, d[t][j][k]);
+	
+	if (res < 0) cout << "0\n";
+	else cout << res << "\n";
 
 	return 0;
-}
-
-int ComputeMax(int time, int change, int tree, int seq[])
-{
-	// handle base cases
-	if (change == )
-
-	if (d[time][change][tree] > 0)
-		return d[time][change][tree];
-
-	return d[time][change][tree] = max(
-		d[time-1][change][tree] + 1,
-		d[time-1][change-1][(tree+1)%2] + 1,
-		d[time-1][change][(tree+1)%2]
-	);
 }
