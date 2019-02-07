95597295b6ff17a38c121606fed73d02b050a356
shnoh171 <shnoh171@gmail.com>
2019-02-07 23:17:04 +0900

    dp

--- backjoon-online-judge/1328-high-building/high_building.cpp
+++ backjoon-online-judge/1328-high-building/high_building.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
-
-// Incomplete
 
 const int mod = 1000000007;
 
 int d[101][101];
-int See(int length, int observe);
 
 int main()
 {
@@ -17,34 +15,19 @@ int main()
 	int n, l, r;
 	cin >> n >> l >> r;
 
-	for (int i = 0; i <= n; ++i) {
-		for (int j = 0; j <= n; ++j) {
-			d[i][j] = -1;
+	long long d[l+1][r+1];
+	memset(d, 0, (l+1)*(r+1)*sizeof(long long));
+	d[1][1] = 1;
+
+	for (int i = 2; i <= n; ++i) {
+		for (int j = l; j >= 1; --j) {
+			for (int k = r; k >= 1; --k) {
+				d[j][k] = ((i-2)*d[j][k] + d[j-1][k] + d[j][k-1]) % mod;
+			}
 		}
 	}
 
-	int res = 0;
-	for (int i = 1; i <= n; ++i) {
-		res = (res + See(i-1, l) + 1) % mod;
-		res = (res + See(n-i, r) + 1) % mod;
-	}
-	
-	cout << res << "\n";
+	cout << d[l][r] << "\n";
 
 	return 0;
-}
-
-int See(int length, int observe) {
-	if (length < observe) return 0;
-	if (length == 0) return 0;
-	if (length == 1 && observe == 1) return 1;
-	if (d[length][observe] >= 0) return d[length][observe];
-
-	int ret = 0;
-	for (int i = 1; i <= length; ++i) {
-		d[i-1][observe] = See(i-1, observe);
-		ret = (ret + d[i-1][observe] + 1) % mod;
-	}
-
-	return ret;
 }
