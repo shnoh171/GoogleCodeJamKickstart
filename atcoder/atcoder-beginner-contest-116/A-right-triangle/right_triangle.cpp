392841372cac055067985c204efff06e6ff05f9f
shnoh171 <shnoh171@gmail.com>
2019-03-12 15:59:12 +0900

    simple

---
+++ atcoder/atcoder-beginner-contest-116/A-right-triangle/right_triangle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int ab, bc, ca;
+	cin >> ab >> bc >> ca;
+
+	cout << ab * bc / 2 << "\n";
+
+	return 0;
+}
